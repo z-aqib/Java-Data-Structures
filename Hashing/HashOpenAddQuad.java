7a7513826bad7d0d6408bbdb07c01cfd59779319
z.aqib <zuha.aqib@gmail.com>
2024-05-29 23:53:51 +0500

    Updating 29.05.24 11:55 PM

--- Hashing/HashOpenAddQuad.java
+++ Hashing/HashOpenAddQuad.java
@@ -1,28 +1,33 @@
 package Hashing;
 
-public class HashingOpenAddQuadratic<T extends Comparable<T>> {
+public class HashOpenAddQuad<T extends Comparable<T>> {
 
-    private Comparable<T>[] table;
-    private int countOccupied;
+    /*
+    This is a hash table data structure. it is NOT resizeable/dynamic. the size is defined at the start. This is used to retrive values in 0(1) and search in 0(1). but this is not always the case - as this hash table is of OPEN ADDRESSING (QUADRATIC). in this, each data value to be added or retrieved gets an index computed based on their data, and it maps it to that index. but, if the index has already been asisgned to a data, the index is Re-Hashed (recomputed) until an empty index is found. 
+     */
+    private Comparable<T>[] table; // the table of values
+    private int countOccupied; // the number of occupied values
 
-    public HashingOpenAddQuadratic(int size) {
+    public HashOpenAddQuad(int size) {
         // constructor: creates a table of 1.33 times of given size and intializes count as 0
+        System.out.println("Welcome to MyHashTable! This HashTable implements Open Addressing in Quadratic Form. ");
         table = (T[]) new Comparable[size + (size / 3)];
         // table size should be a prime number and 1/3 extra.
         this.countOccupied = 0;
+        System.out.println("HashTable of size " + size + " has been created successfully. ");
     }
 
-    public HashingOpenAddQuadratic() {
+    public HashOpenAddQuad() {
         //constructor: passes to next constructor with standard table size 100
         this(10);
     }
 
     @Override
     public String toString() {
-        // method: converts the hashtable into a string
+        // method: converts the hashtable into a string. it takes one line for each index. first it displays the index number and then its value and then takes a new line. 
         String str = "";
-        for (int i = 0; i < table.length;i++) {
-            str += (i+1) + ":'" + table[i] + "'";
+        for (int i = 0; i < table.length; i++) {
+            str += (i + 1) + ":'" + table[i] + "'";
             if (i != table.length - 1) {
                 str = str + " , \n";
             }
@@ -35,7 +40,7 @@ public class HashingOpenAddQuadratic<T extends Comparable<T>> {
         System.out.println(toString());
     }
 
-    public int strToInt(String stringToBeChanged) {
+    private int strToInt(String stringToBeChanged) {
         // method: convert string into integer by summing the ASCII values
         int sum = 0;
         for (int i = 0; i < stringToBeChanged.length(); i++) {
@@ -45,28 +50,20 @@ public class HashingOpenAddQuadratic<T extends Comparable<T>> {
     }
 
     public int Hash(int sum) {
-        //method: hash value calculator. compute hash value by taking mod on table length
+        // method: hash value calculator. compute hash value by taking mod on table length
         return sum % table.length;
     }
 
     public int Rehash(int sum, int i) {
         /* 
-        method: rehash function. 
-        in quadratic addressing, we will square 'i' which is the number of times we 
-        encounter a collision. then we will call hash() function and find the hash of the new 
-        value (value + i^2)
+        method: rehash function. in quadratic addressing, we will square 'i' which is the number of times we encounter a collision. then we will call hash() function and find the hash of the new value (value + i^2)
          */
         return Hash(sum + (int) (Math.pow(i, 2)));
     }
 
     public int insert(Comparable<T> valueToBeInserted) {
         /*
-        method: inserts a value in the table using its hash() value. 
-        call strToInt(v) and change the value to string, save return value in sum.
-        call Hash(sum) and save return value in sum
-        if (no collision on hash value) then place value
-        else call rehash function until empty cell found
-        also compute number of collisions on insertion of each word
+        method: inserts a value in the table using its hash() value. call strToInt(v) and change the value to string, save return value in sum. call Hash(sum) and save return value in sum if (no collision on hash value) then place value else call rehash function until empty cell found also compute number of collisions on insertion of each word. IF a significant number of iterations have been run 
          */
         if (countOccupied == table.length) {
             System.out.println("ERROR: Cannot be inserted as hash table is full. ");
@@ -75,12 +72,16 @@ public class HashingOpenAddQuadratic<T extends Comparable<T>> {
         int sum = strToInt(valueToBeInserted.toString());
         sum = Hash(sum);
         int i = 0; // counts the number of times re-hashed i.e. no. of collisions faced
-        while (this.table[sum] != null) {
+        while (this.table[sum] != null && i < (table.length * 5)) {
             sum = Rehash(sum, ++i);
+        }
+        if (i > table.length) {
+            System.out.println("ERROR: Cannot be inserted as Re-Hash has ran " + i + " times and no available index is found. ");
+            return -1;
         }
         this.table[sum] = valueToBeInserted;
         countOccupied++;
-        System.out.println("SUCCESS: '"+valueToBeInserted+"' successfully inserted. ");
+        System.out.println("SUCCESS: '" + valueToBeInserted + "' successfully inserted at index " + sum + ". ");
         return i;
     }
 
@@ -123,7 +124,7 @@ public class HashingOpenAddQuadratic<T extends Comparable<T>> {
             sum = Rehash(sum, ++i);
         }
         if (table[sum] != null && table[sum].compareTo((T) valueToBeSearched) == 0) {
-            System.out.println("SUCCESS: '" + valueToBeSearched.toString() + "' is found at index "+sum);
+            System.out.println("SUCCESS: '" + valueToBeSearched.toString() + "' is found at index " + sum);
             return true;
         }
         System.out.println("ERROR: '" + valueToBeSearched.toString() + "' could not be found. ");
